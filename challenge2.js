021e89cd4dd937ce6b38ed1ec6b675317a38e901
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-09 12:11:52 -0600

    Add challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,15 @@
+// include comments showing the **overall goal** in your own words, 
+// your **pseudocode**, and your **final solution**. 
+
+// Start with an array of strings with a mix of uppercase and lowercase letters. 
+// Print every word in all lowercase letters.
+
+// array w strings inside - incl upper and lowercase
+// print all words in all lowercase 
+// for statement (loop) to change every string in array
+
+var colors = ["PINK", "yeLLow", "orange", "GrEeN"];
+
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i].toLowerCase())
+};
