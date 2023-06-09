8922c0c6abc412dad84014fb88e0283c222b420a
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-09 12:50:26 -0600

    Add challenge3.js and responses

---
+++ challenge3.js
@@ -0,0 +1,12 @@
+// Write a method or function that removes all instances of 
+// the letter "s" in a string. 
+// The method or function should accept a string as an argument and 
+// return the same string with every instance of the letter 
+// "s" removed.
+
+function removeS(stringHere) {
+        console.log(stringHere.replaceAll("s", ""));
+    };
+
+removeS("pineapples sara squidward.")
+
